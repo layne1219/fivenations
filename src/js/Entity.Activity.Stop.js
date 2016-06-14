7f4501a6567697ce2b1e9b60c22a37f436bd19b7
Bence Varga <vbence86@gmail.com>
2016-06-14 16:50:35 +0100

    Degrading Release Watcher to ES5

---
+++ src/js/Entity.Activity.Stop.js
@@ -0,0 +1,30 @@
+define('Entity.Activity.Stop', ['Entity.Activity'], function(Activity) {
+
+    /**
+     * Constructor function to Stop
+     * @param  {[object]} entity Instance of an Entity class
+     * @return {[object]} 
+     */
+    function Stop(entity) {
+        Activity.call(this);
+        this.entity = entity;
+    }
+
+    Stop.prototype = new Activity;
+    Stop.prototype.constructor = Stop;
+
+    /**
+     * Applying the activity on an entity
+     * @return {[void]}
+     */
+    Stop.prototype.activate = function() {
+        Activity.prototype.activate.call(this);
+        if (this.entity) {
+            this.entity.stop();
+        }
+        this.kill();
+    };
+
+    return Stop;
+
+});
