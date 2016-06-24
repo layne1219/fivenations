149259f6a994441d2327fc737de3d0983dd5894a
Bence Varga <vbence86@gmail.com>
2016-06-24 14:55:22 +0100

    removing ES6 syntax as the Docker file doesn't install ES6 env

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('clean', function () {
 
 gulp.task('pull', function () {
   const branch = 'master';
-  return new Promise((resolve, reject) => {
+  return new Promise(function(resolve, reject){
       git.pull('origin', branch, err => {
           if (err) throw err;
           resolve();
