44ac6ab078e86857ad3fa894e79993ef3f5ea9c1
Bence Varga <vbence86@gmail.com>
2016-08-30 17:35:08 +0100

    added function to fetch entity coordinates in a tiled manner

--- src/js/Map.js
+++ src/js/Map.js
@@ -2,24 +2,43 @@ define('Map', ['Starfield'], function(Starfield) {
 
     // map configration template
     var defaultConfig = {
-            tiles: {
-                tileWidth: 40,
-                tileHeight: 40,
-                width: 32,
-                height: 32
-            },
-            scrollSpeed: 10
+        tiles: {
+            tileWidth: 40,
+            tileHeight: 40,
+            width: 32,
+            height: 32
         },
+        scrollSpeed: 10
+    };
 
-        Map = function(config) {
-            this.init(config);
-        }
+    function Map(config) {
+        this.init(config);
+        this.validateMapConfig();
+    }
 
     Map.prototype = {
 
         init: function(config) {
             this.config = config || defaultConfig;
         },
+
+        validateMapConfig: function() {
+            if (!this.config || !this.config.tiles) {
+                throw 'Invalid config data!';
+            }
+            if (!this.config.tiles.tileWidth) {
+                throw 'Invalid tileWidth property!';
+            }
+            if (!this.config.tiles.tileHeight) {
+                throw 'Invalid tileHeight property!';
+            }
+            if (!this.config.tiles.width) {
+                throw 'Invalid width property!';
+            }
+            if (!this.config.tiles.height) {
+                throw 'Invalid height property!';
+            }
+        },        
 
         setGame: function(game) {
             this.game = game;
@@ -28,7 +47,6 @@ define('Map', ['Starfield'], function(Starfield) {
             game.world.setBounds(0, 0, this.getScreenWidth(), this.getScreenHeight());
 
             this.starfield = new Starfield(this, 0.75);
-
         },
 
         update: function() {
@@ -66,41 +84,27 @@ define('Map', ['Starfield'], function(Starfield) {
         },
 
         getScreenWidth: function() {
-            this.validateMapConfig();
             return this.config.tiles.tileWidth * this.config.tiles.width;
         },
 
         getScreenHeight: function() {
-            this.validateMapConfig();
-            return this.config.tiles.tileWidth * this.config.tiles.width;
+            return this.config.tiles.tileHeight * this.config.tiles.height;
         },
 
         getWidth: function() {
-            this.validateMapConfig();
             return this.config.tiles.width;
         },
 
         getHeight: function() {
-            this.validateMapConfig();
-            return this.config.tiles.width;
+            return this.config.tiles.height;
         },
 
-        validateMapConfig: function() {
-            if (!this.config || !this.config.tiles) {
-                throw 'Invalid config data!';
-            }
-            if (!this.config.tiles.tileWidth) {
-                throw 'Invalid tileWidth property!';
-            }
-            if (!this.config.tiles.tileHeight) {
-                throw 'Invalid tileHeight property!';
-            }
-            if (!this.config.tiles.width) {
-                throw 'Invalid width property!';
-            }
-            if (!this.config.tiles.height) {
-                throw 'Invalid height property!';
-            }
+        getTileByEntity: function(entity) {
+            if (!entity) throw 'Invalid entity was given!';
+            var sprite = entity.getSprite(),
+                x = Math.floor(sprite.x / this.config.tiles.tileWidth),
+                y = Math.floor(sprite.y / this.config.tiles.tileHeight);
+            return [x, y];
         }
 
     }
