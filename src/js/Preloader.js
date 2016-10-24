037aab9a34b31c62450a3eaa78872dc2e57657b7
Bence Varga <vbence86@gmail.com>
2016-10-24 16:06:16 +0100

    extend preloading of syl icons

--- src/js/Preloader.js
+++ src/js/Preloader.js
@@ -23,6 +23,7 @@ define('Preloader', ['Preloader.Entities'], function(preloaderEntities) {
         preloader.load.atlasJSONHash('gui', 'assets/images/gui/GUI_element.png', 'assets/images/gui/GUI_element.json');
         preloader.load.atlasJSONHash('gui.icons.fed', 'assets/images/gui/fed_icons.png', 'assets/images/gui/fed_icons.json');
         preloader.load.atlasJSONHash('gui.icons.ath', 'assets/images/gui/ath_icons.png', 'assets/images/gui/ath_icons.json');
+        preloader.load.atlasJSONHash('gui.icons.syl', 'assets/images/gui/syl_icons.png', 'assets/images/gui/syl_icons.json');
         preloader.load.atlasJSONHash('gui.icons.obj', 'assets/images/gui/obj_icons.png', 'assets/images/gui/obj_icons.json');
     }
 
