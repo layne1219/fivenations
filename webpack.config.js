d5d091e3f89a96e48f53f549b6f27d406b1fcc46
Bence Varga <vbence86@gmail.com>
2017-04-07 16:26:37 +0100

    add pug template for html rendering

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }      
+    ]
+  },
+  entry: [
+    './src/js/main.js',
+    './src/scss/main.scss'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compress: {
+            warnings: false
+        },
+        sourceMap: true
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/pug/app.pug'
+    })
+  ]
+}
