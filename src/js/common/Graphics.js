1e9d57346e1952295a53d8f491140a326c9864f6
Bence Varga <vbence86@gmail.com>
2017-04-07 17:02:57 +0100

    es6 modules + namespace folders

---
+++ src/js/common/Graphics.js
@@ -0,0 +1,60 @@
+let phaserGame;
+let singleton;
+
+function createGraphicsInstance() {
+
+    var groups = {},
+        // layers ordered as follows
+        groupNames = [
+            'starfield',
+            'selectors-buildings',
+            'entities-buildings',
+            'energy-shields-buildings',
+            'selectors',
+            'entities',
+            'energy-shields',
+            'effects',                
+            'prior-gui-elements'
+        ];
+
+    groupNames.forEach(function(name) {
+        groups[name] = phaserGame.add.group();
+    });
+
+    return {
+
+        getGroup: function(id) {
+            if (!id) {
+                throw 'Invalid Id to retrieve a group!';
+            }
+            if (!groups[id]) {
+                throw 'The group cannot be identified through the passed id!';
+            }
+            return groups[id];
+        }
+
+    };
+
+}
+
+export default {
+
+    setGame: function(game) {
+        phaserGame = game;
+    },
+
+    /**
+     * Accessing the singleton instance of the GUI 
+     * @return {object} GUI
+     */
+    getInstance: function() {
+        if (!phaserGame) {
+            throw 'Invoke setGame first to pass the Phaser Game entity!';
+        }
+        if (!singleton) {
+            singleton = createGraphicsInstance();
+        }
+        return singleton;
+    }
+
+};
