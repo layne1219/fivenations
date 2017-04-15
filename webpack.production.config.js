6fed9dcc0865bf7c184bf89f543da4f6e155121d
Bence Varga <vbence86@gmail.com>
2017-04-15 15:36:04 +0100

    production config for webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    app: [
+      'babel-polyfill',
+      path.resolve(__dirname, 'src/js/main.js')
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/js/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.UglifyJsPlugin({
+      drop_console: true,
+      minimize: true,
+      output: {
+        comments: false
+      },
+      sourceMap: true
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
+      { test: /\.scss$/, use: ['sass-loader'] },
+    ]
+  },
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+};
