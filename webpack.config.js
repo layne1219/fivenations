8f745e77aff3939b25d29be252f60d79b3c6ae5f
Bence Varga <vbence86@gmail.com>
2017-04-15 17:23:48 +0100

    Dev server with BrowserSync

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -9,32 +10,30 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './dist/',
+    path: path.resolve(__dirname, 'dist/js'),
+    publicPath: '/js/',
     filename: 'bundle.js'
   },
   devtool: 'source-map',
+  watch: true,
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
       }
     }),
-    /*
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      },
-      sourceMap: true
-    }),*/
+    new BrowserSyncPlugin({
+      host: process.env.IP || 'localhost',
+      port: process.env.PORT || 9000,
+      server: {
+        baseDir: ['./', './dist']
+      }
+    })
   ],
   module: {
     rules: [
+      { test: /(pixi|phaser).js/, use: ['script-loader'] }, 
       { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /\.pug$/, use: ['pug-loader'] },
       { test: /\.scss$/, use: ['sass-loader'] },
     ]
   },
