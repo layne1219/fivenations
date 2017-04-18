6b56291724e1f3fe8ae53c98dba83b70fccf98b8
Bence Varga <vbence86@gmail.com>
2017-04-18 09:44:55 +0100

    ability to create versioned bundles

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,42 +1,45 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const packageJson = require('./package.json');
+const currentVersion = packageJson.version;
+const bundleName = `bundle.${currentVersion}.js`;
 
 module.exports = {
-  entry: {
-    app: [
-      'babel-polyfill',
-      path.resolve(__dirname, 'src/js/browser-connector.js')
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    publicPath: '/js/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      }
-    }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      }
-    }),
-  ],
-  module: {
-    rules: [
-      { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-      { test: /\.scss$/, use: ['sass-loader'] },
-    ]
-  },
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  },
+    entry: {
+        app: [
+            'babel-polyfill',
+            path.resolve(__dirname, 'src/js/browser-connector.js')
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        publicPath: '/js/',
+        filename: bundleName
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            }
+        }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.optimize.UglifyJsPlugin({
+            drop_console: true,
+            minimize: true,
+            output: {
+                comments: false
+            }
+        }),
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
+            { test: /\.scss$/, use: ['sass-loader'] },
+        ]
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
 };
