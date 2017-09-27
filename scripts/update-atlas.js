94f9018d04ac12e422d69e4c0cd5a1e03cc39787
Bence Varga <vbence86@gmail.com>
2017-09-27 17:55:54 +0200

    fix entity icon order

--- scripts/update-atlas.js
+++ scripts/update-atlas.js
@@ -48,7 +48,7 @@ const files = [
 	'fed/fed_build11.json',
 	'fed/fed_build12.json',
 	'fed/fed_build13.json',
-	'fed/fed_build14.json',*/
+	'fed/fed_build14.json',
 
 	'fed/fed_unit01.json',
 	'fed/fed_unit02.json',
@@ -63,7 +63,132 @@ const files = [
 	'fed/fed_unit11.json',
 	'fed/fed_unit12.json',
 	'fed/fed_unit13.json',
-	'fed/fed_unit14.json',	
+	'fed/fed_unit14.json',
+
+	'syl/syl_build01.json',
+	'syl/syl_build02.json',
+	'syl/syl_build03.json',
+	'syl/syl_build04.json',
+	'syl/syl_build05.json',
+	'syl/syl_build06.json',
+	'syl/syl_build07.json',
+	'syl/syl_build08.json',
+	'syl/syl_build09.json',
+	'syl/syl_build10.json',
+	'syl/syl_build11.json',
+	'syl/syl_build12.json',
+	'syl/syl_build13.json',
+	'syl/syl_build14.json',
+
+	'syl/syl_unit01.json',
+	'syl/syl_unit02.json',
+	'syl/syl_unit03.json',
+	'syl/syl_unit04.json',
+	'syl/syl_unit05.json',
+	'syl/syl_unit06.json',
+	'syl/syl_unit07.json',
+	'syl/syl_unit08.json',
+	'syl/syl_unit09.json',
+	'syl/syl_unit10.json',
+	'syl/syl_unit11.json',
+	'syl/syl_unit12.json',
+	'syl/syl_unit13.json',
+	'syl/syl_unit14.json',			
+
+	'tho/tho_build01.json',
+	'tho/tho_build02.json',
+	'tho/tho_build03.json',
+	'tho/tho_build04.json',
+	'tho/tho_build05.json',
+	'tho/tho_build06.json',
+	'tho/tho_build07.json',
+	'tho/tho_build08.json',
+	'tho/tho_build09.json',
+	'tho/tho_build10.json',
+	'tho/tho_build11.json',
+	'tho/tho_build12.json',
+	'tho/tho_build13.json',
+	'tho/tho_build14.json',
+
+	'tho/tho_unit01.json',
+	'tho/tho_unit02.json',
+	'tho/tho_unit03.json',
+	'tho/tho_unit04.json',
+	'tho/tho_unit05.json',
+	'tho/tho_unit06.json',
+	'tho/tho_unit07.json',
+	'tho/tho_unit08.json',
+	'tho/tho_unit09.json',
+	'tho/tho_unit10.json',
+	'tho/tho_unit11.json',
+	'tho/tho_unit12.json',
+	'tho/tho_unit13.json',
+
+	'zho/zho_build01.json',
+	'zho/zho_build02.json',
+	'zho/zho_build03.json',
+	'zho/zho_build04.json',
+	'zho/zho_build05.json',
+	'zho/zho_build06.json',
+	'zho/zho_build07.json',
+	'zho/zho_build08.json',
+	'zho/zho_build09.json',
+	'zho/zho_build10.json',
+	'zho/zho_build11.json',
+	'zho/zho_build12.json',
+	'zho/zho_build13.json',
+	'zho/zho_build14.json',
+	'zho/zho_build15.json',
+
+	'zho/zho_unit01.json',
+	'zho/zho_unit02.json',
+	'zho/zho_unit03.json',
+	'zho/zho_unit04.json',
+	'zho/zho_unit05.json',
+	'zho/zho_unit06.json',
+	'zho/zho_unit07.json',
+	'zho/zho_unit08.json',
+	'zho/zho_unit09.json',
+	'zho/zho_unit10.json',
+	'zho/zho_unit11.json',
+	'zho/zho_unit12.json',
+	'zho/zho_unit13.json',
+	'zho/zho_unit14.json',
+	'zho/zho_unit15.json',
+	'zho/zho_unit16.json', */
+
+	'ast/asteroid01.json',
+	'ast/asteroid02.json',
+	'ast/asteroid03.json',
+	'ast/asteroid04.json',
+
+	'ast/asteroid_sm01.json',
+	'ast/asteroid_sm02.json',
+	'ast/asteroid_sm03.json',
+	'ast/asteroid_sm04.json',
+	
+	'ast/asteroid_big01.json',
+	'ast/asteroid_big02.json',
+	
+	'ast/asteroid_ice01.json',
+	'ast/asteroid_ice02.json',
+	'ast/asteroid_ice_sm01.json',
+	'ast/asteroid_ice_sm02.json',
+
+	'ast/asteroid_silicon01.json',
+	'ast/asteroid_silicon02.json',
+	'ast/asteroid_silicon_sm01.json',
+	'ast/asteroid_silicon_sm02.json',
+
+	'ast/asteroid_titanium01.json',
+	'ast/asteroid_titanium02.json',
+	'ast/asteroid_titanium_sm01.json',
+	'ast/asteroid_titanium_sm02.json',
+
+	'ast/asteroid_uranium01.json',
+	'ast/asteroid_uranium02.json',
+	'ast/asteroid_uranium_sm01.json',
+	'ast/asteroid_uranium_sm02.json',
 
 ];
 
@@ -83,8 +208,14 @@ files.forEach(file => {
 			];*/
 			const newData = Object.assign({}, data);
 			const tmp = Object.assign({}, newData.frames[1]);
-			newData.frames[1] = Object.assign({}, newData.frames[2]);
-			newData.frames[2] = tmp; 
+
+			newData.frames.unshift(tmp);
+
+			/*if (tmp.filename.indexOf('_sm_') !== -1) {
+				newData.frames[1] = Object.assign({}, newData.frames[2]);
+				newData.frames[2] = tmp; 
+			}*/
+
 			return jsonFile.writeFile(target, newData, {spaces: 2});
 		})
 		.then((err, data) => console.log(`${target} has been updated`));
