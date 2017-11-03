479506be0eac7b07167aa398fc45d114b3a09a8a
vbence86 <vbence86@gmail.com>
2017-11-03 21:11:45 +0100

    add logic to take accuracy into consideration

--- src/js/common/Const.js
+++ src/js/common/Const.js
@@ -65,6 +65,8 @@ export const ENTITY_ICON_SMALL_DIMENSIONS = {
 
 // Weapon constants
 export const WEAPON_INSTANCE_DELAY = 10;
+export const WEAPON_ACCURACY_SPREAD_IN_DEGREES = 60;
+export const WEAPON_ACCURACY_SPREAD_IN_RADIAN = Phaser.Math.degToRad(WEAPON_ACCURACY_SPREAD_IN_DEGREES);
 
 export const GUI_BUTTON = {
     spritesheet: {
