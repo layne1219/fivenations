a1f6a68f8ffb55ca4ee3a589ceca942110abed75
Bence Varga <vbence86@gmail.com>
2018-01-11 09:55:11 +0100

    #358 Complete AudioManager implementation

--- src/js/audio/AudioManager.js
+++ src/js/audio/AudioManager.js
@@ -1,13 +1,87 @@
+/* eslint class-methods-use-this: 0 */
+import EventEmitter from '../sync/EventEmitter';
+import { PreloadedSprites, Sprites } from './Sprites';
+import { Events } from './Events';
+
+const audioSprites = {};
+
 let phaserGame;
 let singleton;
 
+/**
+ * Manager class that is built around Phaser.Game Audio engine. It
+ * is responsable to initialise the AudioSprites and register
+ * event listeners for locally (not synced for multiplayer)
+ * emitted events.
+ */
 class AudioManager {
-  constructor(game) {
-    this.game = game;
+  /**
+   * Initialises Preloaded AudioSprites and registers event listeners
+   */
+  constructor() {
+    this.initPreloadedAudioSprites();
+    this.addEventListeners();
   }
 
-  addEventListeners(emitter) {
-    // emitter.addEventListeners('');
+  /**
+   * Loads Preloaded AudioSprites from Phaser Asset cache
+   * for further playback
+   */
+  initPreloadedAudioSprites() {
+    PreloadedSprites.forEach(key => this.loadAudioSprite(key));
+  }
+
+  /**
+   * Loads the AudioSprite and defines AudioFrames according
+   * to the given marker definitions from Sprites.js
+   */
+  loadAudioSprite(key) {
+    audioSprites[key] = phaserGame.add.audio(key);
+    audioSprites[key].allowMultiple = true;
+    Sprites[key].markers.forEach((marker) => {
+      const data = Sprites[key].markers[marker];
+      audioSprites[key].addMarker(
+        marker,
+        data.start,
+        data.duration,
+        data.volume,
+        data.loop,
+      );
+    });
+  }
+
+  /**
+   * Registers event listener predefined in Events.js
+   */
+  addEventListeners() {
+    const dispatcher = EventEmitter.getInstance().local;
+    Object.keys(Events).forEach((key) => {
+      this.bindListener(dispatcher, key, Events[key]);
+    });
+  }
+
+  /**
+   * Links default or the given event listener to defined event
+   * @param {object} emitter - EventDispatcher instance that shares
+   * events across the complete game scope
+   * @param {string} event - name of the event (e.g.: audio/test)
+   * @param {object} options - details of event dispatching
+   */
+  bindListener(emitter, event, options) {
+    const defaultCallback = () => this.playAudioSprite(options);
+    const callback = options.callback || defaultCallback;
+    emitter.addEventListeners(event, callback.bind(this));
+  }
+
+  /**
+   * Triggers the audio frame to be played through Phaser.Game
+   * Audio engine
+   * @param {object} options - details of the playback
+   */
+  playAudioSprite(options) {
+    const { key, marker } = options;
+    if (!audioSprites[key]) return;
+    audioSprites[key].play(marker);
   }
 }
 
---
+++ src/js/audio/Sprites.js
@@ -0,0 +1,45 @@
+export const PreloadedSprites = ['sfx'];
+
+export const Sprites = {
+  sfx: {
+    asset: 'assets/audio/sfx.ogg',
+    markers: {
+      'alien death': {
+        start: 1,
+        duration: 1.0,
+      },
+      'boss hit': {
+        start: 3,
+        duration: 0.5,
+      },
+      escape: {
+        start: 4,
+        duration: 3.2,
+      },
+      meow: {
+        start: 8,
+        duration: 0.5,
+      },
+      numkey: {
+        start: 9,
+        duration: 0.1,
+      },
+      ping: {
+        start: 10,
+        duration: 1.0,
+      },
+      death: {
+        start: 12,
+        duration: 4.2,
+      },
+      shot: {
+        start: 17,
+        duration: 1.0,
+      },
+      squit: {
+        start: 19,
+        duration: 0.3,
+      },
+    },
+  },
+};
