65b03d3a5650a63a6779e64ee0f0f5fdc3545ef9
Bence Varga <vbence86@gmail.com>
2018-02-08 23:16:06 +0100

    functionality to preload default maps

---
+++ src/js/preloader/Maps.js
@@ -0,0 +1,15 @@
+const { PUBLIC_URL } = process.env;
+// default maps to be preloaded regardless of the game mode
+const DEFAULT_MAPS = ['promotionalmap01-1.json'];
+
+export default {
+  /**
+   * Loads all available translation files
+   * @param {object} preloader - Phaser Preloader object
+   */
+  load(preloader) {
+    DEFAULT_MAPS.forEach((key) => {
+      preloader.load.json(key, `${PUBLIC_URL}/assets/maps/${key}.json`);
+    });
+  },
+};
