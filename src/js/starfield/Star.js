0f4ec2e41d993a658f8948821f192b122e9e4ce5
Bence Varga <vbence86@gmail.com>
2018-02-18 20:17:55 +0100

    finalise stars

--- src/js/starfield/Star.js
+++ src/js/starfield/Star.js
@@ -2,21 +2,51 @@
 import SpaceObject from './SpaceObject';
 
 const ns = window.fivenations;
-const { width, height } = ns.window;
 
+/**
+ * SpaceObject that is repositioned when gets off screen
+ */
 class Star extends SpaceObject {
+  /**
+   * Returns an instance of Star class that will be automatically
+   * repositioned when gets off the predefined screen dimensions
+   */
+  constructor(sprite) {
+    super(sprite);
+    this.setOffScreenDimensions();
+  }
+
+  /**
+   * Sets the current window dimensions that is used to determine
+   * whether the star is off screen or not
+   */
+  setOffScreenDimensions() {
+    const { width, height } = ns.window;
+    this.offScreen = {
+      width,
+      height,
+    };
+  }
+
+  /**
+   * Updates the Star at every tick when the parent layer is considered
+   * "dirty" and then repositions it randomly
+   * @param {object} texture - Phaser.Texture
+   * @param {game} game - Phaser.Game
+   * @param {boolean} clear - whether the texture must be cleared
+   */
   update(texture, game, clear) {
     super.update(texture, game, clear);
 
-    // assessing whether the star is drifted off screen
+    // assesses whether the star is drifted off screen
     if (this.x + (this.sprite.width - game.camera.x * this.z) < 0) {
-      this.setX(this.x + this.sprite.width + width);
-    } else if (this.x - game.camera.x * this.z > width) {
-      this.setX(this.x - this.sprite.width - width);
+      this.setX(this.x + this.sprite.width + this.offScreen.width);
+    } else if (this.x - game.camera.x * this.z > this.offScreen.width) {
+      this.setX(this.x - this.sprite.width - this.offScreen.width);
     } else if (this.y + (this.sprite.height - game.camera.y * this.z) < 0) {
-      this.setY(this.y + this.sprite.height + height);
-    } else if (this.y - game.camera.y * this.z > height) {
-      this.setY(this.y - this.sprite.height - height);
+      this.setY(this.y + this.sprite.height + this.offScreen.height);
+    } else if (this.y - game.camera.y * this.z > this.offScreen.height) {
+      this.setY(this.y - this.sprite.height - this.offScreen.height);
     }
   }
 }
--- src/js/starfield/StarGenerator.js
+++ src/js/starfield/StarGenerator.js
@@ -3,45 +3,45 @@ import Star from './Star';
 import SpaceObjectGenerator from './SpaceObjectGenerator';
 import Util from '../common/Util';
 
-const NUMBER_OF_STARS = 7;
-const NUMBER_OF_STARS_PER_SCREEN = 10;
+const NUMBER_OF_STAR_TYPES = 7;
+const NUMBER_OF_STARS_PER_SCREEN = 50;
 const ns = window.fivenations;
-const { width, height } = ns.window;
 let sprite;
 
 function getRandomizedZ() {
-  const z = Math.min(Math.random() + 0.5, 0.95);
+  const z = Math.min(Math.random() + 0.05, 0.5);
   return z;
 }
 
-function createStar() {
-  const z = getRandomizedZ();
-  const star = new Star()
-    .setX(Util.rnd(0, width))
-    .setY(Util.rnd(0, height))
-    .setZ(z)
-    .setSprite(sprite)
-    .setFrame(Util.rnd(0, NUMBER_OF_STARS - 1));
-
-  return star;
-}
-
 class StarGenerator extends SpaceObjectGenerator {
-  constructor(game) {
-    super(game);
-    this.createSprites();
+  generate() {
     this.createStars();
   }
 
-  createSprite() {
-    if (sprite) return;
-    sprite = this.game.make.sprite(0, 0, 'starfield.stars');
+  createRandomStarSprite() {
+    const sprite = this.deepSpaceLayer.getSprite('starfield.stars');
+    const phaserGame = ns.game.game;
+    const starFrame = Util.rnd(0, NUMBER_OF_STAR_TYPES - 1);
+    sprite.frame = starFrame;
+    const clone = phaserGame.make.sprite(0, 0, sprite.generateTexture());
+    return clone;
+  }
+
+  createStar() {
+    const { width, height } = ns.window;
+    const starSprite = this.createRandomStarSprite();
+    const z = getRandomizedZ();
+    const star = new Star(starSprite)
+      .setX(Util.rnd(0, width))
+      .setY(Util.rnd(0, height))
+      .setZ(z);
+
+    return star;
   }
 
   createStars() {
-    let star;
     for (let i = 0; i < NUMBER_OF_STARS_PER_SCREEN; i += 1) {
-      star = createStar();
+      const star = this.createStar();
       this.addSpaceObject(star);
     }
   }
