59ec7435c40139503369b10711e91f1521351610
Bence Varga <vbence86@gmail.com>
2018-04-22 22:56:56 +0200

    #467 Add Mine Button to control panel

---
+++ src/js/gui/MineButton.js
@@ -0,0 +1,32 @@
+import EventEmitter from '../sync/EventEmitter';
+import SelectCoords from './SelectCoords';
+import ActivityManager from './ActivityManager';
+
+export default {
+  activate(entityManager, controlPanel) {
+    const activity = ActivityManager.getInstance().start(SelectCoords);
+    activity.on('select', () => {
+      let targetEntity;
+      entityManager.entities().filter((entity) => {
+        if (entity.isHover()) {
+          targetEntity = entity;
+          return true;
+        }
+        return false;
+      });
+
+      if (targetEntity) {
+        EventEmitter.getInstance()
+          .synced.entities(':user:selected')
+          .mine({
+            targetEntity,
+          });
+        targetEntity.selectedAsTarget();
+      }
+
+      controlPanel.selectMainPage();
+    });
+
+    controlPanel.selectCancelPage();
+  },
+};
