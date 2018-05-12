c00b23873648b73900afa8af0063c372479d5546
Bence Varga <vbence86@gmail.com>
2018-05-12 09:00:28 +0200

    #498 fix linting issues

--- src/js/gui/ProductionTab.js
+++ src/js/gui/ProductionTab.js
@@ -16,6 +16,76 @@ const SLOTS_PADDING_Y = 5;
 const frames = {};
 
 /**
+ * Creates sprites for the production slot icons to the
+ * and appends them to given sprite collection
+ * @param {object} sprites
+ */
+function addSlotIcons(sprites) {
+  const slotNamePrefix = 'slot-';
+  for (let i = 0; i < SLOTS_COUNT - 1; i += 1) {
+    const sprite = this.createElement({
+      assetId: 'gui',
+      id: `XXX_gui_construction_queue_${i + 1}.png`,
+    });
+    sprites[`${slotNamePrefix}${i}`] = sprite;
+  }
+}
+
+/**
+ * Creates sprites for all the entity icons and appends them to
+ * given sprite collection
+ * @param {object} sprites
+ */
+function addEntityIcons(sprites) {
+  Object.keys(ns.entities).forEach((key) => {
+    const sprite = this.createElement({
+      assetId: key,
+      idx: ENTITY_ICON_CONSTRUCT,
+    });
+    sprites[key] = sprite;
+  });
+}
+
+/**
+ * Pieces all the given sprites together into spritesheet. This is
+ * because the icons of entities are placed into separate spritesheets and
+ * in order to keep the rendering of the production tab efficient we
+ * merge them together into one spritesheet.
+ * @param {object} game - Phaser instance
+ * @param {object} sprites - collection of Phaser.Sprites
+ */
+function constructSpriteSheet(game, sprites) {
+  const spriteCount = Object.keys(sprites).length;
+  const columns = 25;
+  const firstKey = Object.keys(sprites)[0];
+  const spriteWidth = sprites[firstKey].width;
+  const spriteHeight = sprites[firstKey].height;
+  const rows = Math.floor(spriteCount / columns);
+  const width = Math.floor(spriteWidth * columns);
+  const height = Math.floor(spriteHeight * rows);
+  const bmd = game.add.bitmapData(width, height);
+
+  Object.keys(sprites).forEach((key, idx) => {
+    const sprite = sprites[key];
+    const x = spriteWidth * (idx % columns);
+    const y = spriteHeight * Math.floor(idx / columns);
+    bmd.draw(sprite, x, y);
+    frames[key] = idx;
+  });
+
+  game.cache.addSpriteSheet(
+    SPRITESHEET_ID,
+    '',
+    bmd.canvas,
+    spriteWidth,
+    spriteHeight,
+    spriteCount,
+    0,
+    0,
+  );
+}
+
+/**
  * Phaser.Group class that realises the Production Slot element
  */
 class ProductionTab extends Phaser.Group {
@@ -49,6 +119,8 @@ class ProductionTab extends Phaser.Group {
     for (let i = 0; i < SLOTS_COUNT; i += 1) {
       const button = new ProductionTabButton(this.game);
 
+      // according to the layout the first two slots must be
+      // below one another and the rest spread out horizontally
       if (i === 0) {
         button.x = 0;
         button.y = 0;
@@ -68,55 +140,9 @@ class ProductionTab extends Phaser.Group {
    */
   createProductionTabButtonSpriteSheet() {
     const sprites = {};
-    let spriteCount = 0;
-
-    // add slot frames
-    const slotNamePrefix = 'slot-';
-    for (let i = 0; i < SLOTS_COUNT - 1; i += 1) {
-      const sprite = this.createElement({
-        assetId: 'gui',
-        id: `XXX_gui_construction_queue_${i + 1}.png`,
-      });
-      sprites[`${slotNamePrefix}${i}`] = sprite;
-      spriteCount += 1;
-    }
-
-    // add entity icons
-    Object.keys(ns.entities).forEach((key) => {
-      const sprite = this.createElement({
-        assetId: key,
-        idx: ENTITY_ICON_CONSTRUCT,
-      });
-      sprites[key] = sprite;
-      spriteCount += 1;
-    });
-
-    const columns = 25;
-    const spriteWidth = sprites[0].width;
-    const spriteHeight = sprites[0].height;
-    const rows = Math.floor(spriteCount / columns);
-    const width = Math.floor(spriteWidth * columns);
-    const height = Math.floor(spriteHeight * rows);
-    const bmd = this.game.add.bitmapData(width, height);
-
-    Object.keys(sprites).forEach((key, idx) => {
-      const sprite = sprites[key];
-      const x = spriteWidth * (idx % columns);
-      const y = spriteHeight * Math.floor(idx / columns);
-      bmd.drawSprite(sprite, x, y);
-      frames[key] = idx;
-    });
-
-    this.game.cache.addSpriteSheet(
-      SPRITESHEET_ID,
-      '',
-      bmd.canvas,
-      spriteWidth,
-      spriteHeight,
-      spriteCount,
-      0,
-      0,
-    );
+    addSlotIcons(sprites);
+    addEntityIcons(sprites);
+    constructSpriteSheet(this.game, sprites);
   }
 
   /**
