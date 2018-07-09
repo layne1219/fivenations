7cac3805f011bba683fa620f295faa516daebce6
Bence Varga <vbence86@gmail.com>
2018-07-09 11:53:06 +0200

    #632 refine appearance of proximity radious

--- src/js/gui/ProximityMonitor.js
+++ src/js/gui/ProximityMonitor.js
@@ -4,6 +4,8 @@ import EventEmitter from '../sync/EventEmitter';
 
 const ns = window.fivenations;
 
+const GRADIENT_EDGE = 50;
+
 class ProximityMonitor extends Phaser.Group {
   constructor() {
     super(ns.game.game);
@@ -16,10 +18,12 @@ class ProximityMonitor extends Phaser.Group {
    * @param {object} entity - Entity instance
    */
   createProximityDisplay(entity) {
-    const width = entity.getDataObject().getConstructionProximity();
+    const radius = entity.getDataObject().getConstructionProximity();
+    const width = (radius + GRADIENT_EDGE) * 2;
     this.bmd = this.game.add.bitmapData(width, width);
     this.sprite = this.bmd.addToWorld(0, 0);
     this.sprite.alpha = 0.4;
+    this.sprite.anchor.set(0.5);
     this.add(this.sprite);
 
     this.drawProximityDisplay(entity);
@@ -31,14 +35,26 @@ class ProximityMonitor extends Phaser.Group {
    */
   drawProximityDisplay(entity) {
     const ctx = this.bmd.context;
-    const width = entity.getDataObject().getConstructionProximity();
-    const cx = width / 2;
-    const r = cx * 0.75;
-    const gradient = ctx.createRadialGradient(cx, cx, r, cx, cx, cx);
+    const radius = entity.getDataObject().getConstructionProximity();
+    const cx = radius + GRADIENT_EDGE;
+    const gradient = ctx.createRadialGradient(
+      cx,
+      cx,
+      radius,
+      cx,
+      cx,
+      radius + GRADIENT_EDGE,
+    );
     gradient.addColorStop(0, '#77C7D2');
     gradient.addColorStop(1, 'rgba(119, 199, 210, 0)');
     ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, width, width);
+    ctx.fillRect(0, 0, cx * 2, cx * 2);
+
+    ctx.beginPath();
+    ctx.arc(cx, cx, radius, 0, 2 * Math.PI, false);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#81DBE6';
+    ctx.stroke();
   }
 
   /**
@@ -74,6 +90,7 @@ class ProximityMonitor extends Phaser.Group {
     // the sprite is not a child of the entity for various overlapping issues
     // therefore it needs to follow it upon every tick
     this.update = function update() {
+      if (!this.visible) return;
       this.x = entity.getSprite().x;
       this.y = entity.getSprite().y;
     };
